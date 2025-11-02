3bd3e11f619e32514d6a46f6ccf2c02876c6d20c
“Jayakeerthan143” <jayakeerthan143@gmail.com>
2025-11-02 12:24:11 +0530

    Fix CORS for Vercel deployment

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://handicraft-marketplace-alpha.vercel.app",
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
